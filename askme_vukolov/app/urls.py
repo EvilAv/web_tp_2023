4a4066a4eab54f71425506b817af22d426804fd7
EvilAv <andrey.vookolov85@gmail.com>
2023-11-04 02:37:08 +0300

    basic setup

--- askme_vukolov/app/urls.py
+++ askme_vukolov/app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from app import views
+
 urlpatterns = [
-    path('', include('app.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
