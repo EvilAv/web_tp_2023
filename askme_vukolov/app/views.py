3dea839eec976f2fbe632c704a375ba61e9fd0ff
EvilAv <andrey.vookolov85@gmail.com>
2023-11-04 12:34:11 +0300

    add paginator

--- askme_vukolov/app/views.py
+++ askme_vukolov/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.paginator import Paginator, PageNotAnInteger
 
 QUESTIONS_DB = [
     {
@@ -36,9 +36,31 @@ def get_top_tags(rows=3):
     return res
 
 
+def paginate(objects_list, request, per_page=10):
+    p = Paginator(objects_list, per_page)
+    page_idx = request.GET.get('page', 1)
+    try:
+        page_idx = int(page_idx)
+    except ValueError:
+        page_idx = 1
+    if page_idx < 1:
+        page_idx = 1
+    elif page_idx > p.num_pages:
+        page_idx = p.num_pages
+    raw_page = p.page(page_idx)
+    correct_page_range = raw_page.paginator.get_elided_page_range(page_idx, on_each_side=2, on_ends=1)
+    return {
+        'page_obj': raw_page,
+        'obj_list': raw_page.object_list,
+        'page_range': correct_page_range,
+        }
+
+
 def index(request):
-    return render(request, 'index.html', {'questions': QUESTIONS_DB,
-                                          'tags': get_top_tags(), 'is_logged_in': True})
+    page = paginate(QUESTIONS_DB, request, 7)
+    return render(request, 'index.html', {'questions': page['obj_list'],
+                                          'tags': get_top_tags(), 'is_logged_in': True,
+                                          'page': page})
 
 
 def hot(request):
@@ -47,13 +69,17 @@ def hot(request):
 
 
 def detail_question(request, id):
-    return render(request, 'question.html', {'question': QUESTIONS_DB[id], 'answers': ANSWERS_DB,
-                                             'tags': get_top_tags(), 'is_logged_in': True})
+    page = paginate(ANSWERS_DB, request, 7)
+    return render(request, 'question.html', {'question': QUESTIONS_DB[id], 'answers': page['obj_list'],
+                                             'tags': get_top_tags(), 'is_logged_in': True,
+                                             'page': page})
 
 
 def questions_by_tag(request, slag):
-    return render(request, 'tag.html', {'questions': QUESTIONS_DB, 'tag_name': slag,
-                                        'tags': get_top_tags(), 'is_logged_in': True})
+    page = paginate(QUESTIONS_DB, request, 7)
+    return render(request, 'tag.html', {'questions': page['obj_list'], 'tag_name': slag,
+                                        'tags': get_top_tags(), 'is_logged_in': True,
+                                        'page': page})
 
 
 def login(request):
