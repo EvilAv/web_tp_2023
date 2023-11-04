2453c46fa6c1b0108dbb6bb7d92e2f09f977dc67
EvilAv <andrey.vookolov85@gmail.com>
2023-11-04 03:38:00 +0300

    add questions and tags

--- askme_vukolov/app/urls.py
+++ askme_vukolov/app/urls.py
@@ -5,4 +5,8 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hot', views.hot, name='hot'),
+    path('question/<int:id>', views.detail_question, name='detail_question'),
+    path('tag/<str:slag>', views.questions_by_tag, name='tag'),
+    path('login', views.login, name='login'),
 ]
--- askme_vukolov/app/views.py
+++ askme_vukolov/app/views.py
@@ -2,5 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+QUESTIONS_DB = [
+    {
+        'id': i,
+        'title': 'Question #' + str(i),
+        'text': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti incidunt fuga dicta eaque, quae impedit!'
+    } for i in range(50)
+]
+
+ANSWERS_DB = [
+    {
+        'title': 'Answer #' + str(i),
+        'text': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti incidunt fuga dicta eaque, quae impedit!',
+        'rate': str(i - 10),
+    } for i in range(50)
+]
+
+
 def index(request):
-    return render(request, 'layouts/base.html')
+    return render(request, 'index.html', {'questions': QUESTIONS_DB})
+
+
+def hot(request):
+    return render(request, 'index.html', {'questions': QUESTIONS_DB})
+
+
+def detail_question(request, id):
+    return render(request, 'question.html', {'question': QUESTIONS_DB[id], 'answers': ANSWERS_DB})
+
+
+def questions_by_tag(request, slag):
+    return render(request, 'tag.html', {'questions': QUESTIONS_DB, 'tag_name': slag})
+
+def login(request):
+    pass
