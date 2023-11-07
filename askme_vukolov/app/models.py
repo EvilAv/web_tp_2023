d71b12d965b1de498facf717c6343f51404cad54
EvilAv <andrey.vookolov85@gmail.com>
2023-11-07 23:14:33 +0300

    fix models

--- askme_vukolov/app/models.py
+++ askme_vukolov/app/models.py
@@ -24,12 +24,25 @@ class Question(models.Model):
 
 class Rate(models.Model):
     RATE_TYPES = [
-        ('l', 'like'),
-        ('d', 'dislike'),
+        (1, 'like'),
+        (-1, 'dislike'),
     ]
-    type = models.CharField(max_length=1, choices=RATE_TYPES)
+    type = models.IntegerField(choices=RATE_TYPES)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     parent = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        # prevent multiple rates from one user to one question
+        if self.parent.rate_set.filter(author=self.author).exists():
+            return
+        super().save(*args, **kwargs)
+        self.parent.cur_rate += self.type
+        self.parent.save()
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+        self.parent.cur_rate -= self.type
+        self.parent.save()
 
 
 class Answer(models.Model):
@@ -38,3 +51,15 @@ class Answer(models.Model):
     title = models.CharField(max_length=256)
     text = models.TextField(max_length=2048)
     date_time = models.DateTimeField(auto_now_add=True)
+
+    # dp this in case someone very smart somehow break the data
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.parent.ans_count += 1
+        self.parent.save()
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+        self.parent.ans_count -= 1
+        self.parent.save()
+
